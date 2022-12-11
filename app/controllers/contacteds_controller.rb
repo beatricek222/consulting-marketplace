5f4cba20786f05a3b50567f9d0b14ce31d30309c
beatricek222 <beatricek222@gmail.com>
2022-12-11 03:57:02 +0000

    Email functionality complete

--- app/controllers/contacteds_controller.rb
+++ app/controllers/contacteds_controller.rb
@@ -33,25 +33,26 @@ class ContactedsController < ApplicationController
     subject = params.fetch("query_subject")
     email_body = params.fetch("query_email")
 
-    # Get your credentials from your Mailgun dashboard, or from Canvas if you're using mine
-    mg_api_key = "your-api-key"
-    mg_sending_domain = "your-sending-domain"
-
-    # Create an instance of the Mailgun Client and authenticate with your API key
-    mg_client = Mailgun::Client.new(mg_api_key)
-
-    # Craft your email as a Hash literal with these four keys
-    email_info = {
-      :from => @current_user.email,
-      :to => other_email,  # Put your own email address here if you want to see it in action
-      :subject => subject,
-      :text => email_body,
-    }
-
-    # Send your email!
-    mg_client.send_message(mg_sending_domain, email_info)
-
     if the_contacted.valid?
+
+      # Get your credentials from your Mailgun dashboard, or from Canvas if you're using mine
+      mg_api_key = ENV.fetch("MAILGUN_API_KEY")
+      mg_sending_domain = ENV.fetch("MAILGUN_SENDING_DOMAIN")
+
+      # Create an instance of the Mailgun Client and authenticate with your API key
+      mg_client = Mailgun::Client.new(mg_api_key)
+
+      # Craft your email as a Hash literal with these four keys
+      email_info = {
+        :from => @current_user.email,
+        :to => other_email,  # Put your own email address here if you want to see it in action
+        :subject => subject,
+        :text => email_body,
+      }
+
+      # Send your email!
+      mg_client.send_message(mg_sending_domain, email_info)
+
       the_contacted.save
       redirect_to("/contacteds", { :notice => "Contacted successfully." })
     else
